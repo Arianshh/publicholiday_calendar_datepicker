ac2d695ea338701506bfd94ebc546798da0e430a
Arianshh <arian.shariat@gmail.com>
2023-05-10 18:07:15 +0330

    Highlight holidays in calendar V1.0

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# Copyright 2023 Arian Shariat <arian.shariat@gmail.com>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Public Holiday Calendar",
+    "summary": "Public holidays color changes in datepicker and calendar.",
+    "version": "15.0",
+    "license": "LGPL-3",
+    "category": "custom",
+    "author": "Arian Shariat",
+    "website": "https://github.com/Arianshh",
+    "depends": ["hr_holidays"],
+    "css": ['static/src/css/style.css'],
+    "price": 19.99,
+    'currency': 'USD',
+    "data": [
+        "data/data.xml",
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'publicholiday_calendar_datepicker/static/src/css/style.css',
+        ],
+        'web.assets_common': [
+            "publicholiday_calendar_datepicker/static/src/legacy/js/libs/fullcalendar.js",
+            "publicholiday_calendar_datepicker/static/lib/tempusdomainus.js/tempusdomainus.js",
+
+        ],
+    },
+
+    "installable": True,
+    "application": True,
+}
---
+++ controllers/main.py
@@ -0,0 +1,22 @@
+import json
+
+from odoo import http
+from odoo.http import request
+
+
+class HolidayControllers(http.Controller):
+    @http.route('/publicholiday_calendar_datepicker/get_holidays', auth='public', type='http', methods=['GET'])
+    def get_public_holidays(self):
+        holidays = request.env['resource.calendar.leaves'].search([('resource_id', '=', False)])
+        return request.make_response(
+            data=json.dumps([{'date': str(h.date_from)} for h in holidays]),
+            headers=[('Content-Type', 'application/json')]
+        )
+
+    @http.route('/publicholiday_calendar_datepicker/is_holiday', type='http', auth='public', website=True)
+    def is_holiday(self, event_id):
+        holiday_id = request.env['resource.calendar.leaves'].sudo().search([('meeting_id', '=', int(event_id))],                                                                           limit=1)
+        return request.make_response(
+            data=json.dumps(bool(holiday_id)),
+            headers=[('Content-Type', 'application/json')]
+        )
